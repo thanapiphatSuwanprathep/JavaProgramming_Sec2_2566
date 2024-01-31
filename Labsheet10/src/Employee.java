0b4e038b677e95e93abe9ab6af7d6aba54e08af6
ThanapiphatSuwanprathep <Big26102547@gmail.com>
2024-01-31 11:15:23 +0700

    31_1_2024

--- Labsheet10/src/Employee.java
+++ Labsheet10/src/Employee.java
@@ -1,4 +1,27 @@
 
 public class Employee {
+	private String id;
+	private String name;
+	private double salary;
+	
+	public Employee(String id,String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		
+	}
+	public Employee(String id,String namr) {
+		this.id = id;
+		this.name = name;
+	}
+	public String getID() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getSalary() {
+		return salary;
+	}
 
 }
--- Labsheet10/src/sales.java
+++ Labsheet10/src/sales.java
@@ -1,4 +1,27 @@
-
-public class sales {
+import java.text.*;
+public class sales extends Employee{
+	private double sales;
+	
+	DecimalFormat frm = new DecimalFormat("#,###.00");
+	public sales(String id,String name, double salary,double sales) {
+		super(id,name,salary);
+		this.sales = sales;
+	}
+	public sales(String id,String name) {
+		super(id,name);
+		this.sales = 0;
+	}
+	public double getCommission() {
+		if(sales<10000) {
+			return 0;
+		}else if(sales<50000) {
+			return(sales - 10000)* 0.10;
+		}else {
+			return(sales - 50000)* 0.15;
+		}
+	}
+	public String toString() {
+		return super.getName()+"("+super.getID()+")get commission"+frm.format(getCommission()+ "B.");
+	}
 
 }
