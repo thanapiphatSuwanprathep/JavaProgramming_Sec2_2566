7adc2e4442298d1417fc62340d117fdfdd06257d
ThanapiphatSuwanprathep <Big26102547@gmail.com>
2024-02-14 11:20:57 +0700

    14-2-67

--- Labsheet12/src/lab1201.java
+++ Labsheet12/src/lab1201.java
@@ -1,9 +1,21 @@
-
-public class lab1201 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.*;
+import java.io.*;
+public class lab1201
+{
+	public static void main(String[] args) throws IOException
+	{
+		// use Scanner Class read data from file
+		Scanner readFile = new Scanner(new File("d://txtFile//MemberLogin.txt"));
+		while(readFile.hasNext())
+		{
+			String fname = readFile.next();
+			String lname = readFile.next(); //readFile.next();
+			readFile.next();
+			String email = readFile.next().toUpperCase();
+			lname.charAt(0);
+			
+			System.out.println(lname.charAt(0)+"."+fname + " " + "(" + email + ")");
+		}
+		readFile.close();
 	}
-
 }
--- Labsheet12/src/lab1202.java
+++ Labsheet12/src/lab1202.java
@@ -1,9 +1,40 @@
-
-public class lab1202 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.*;
+import java.io.*;
+public class lab1202
+{
+	public static void main(String[] args) throws IOException
+	{
+		Scanner scan= new Scanner(System.in);
+		Scanner readFile = new Scanner(new File("d://txtFile//MemberLogin.txt"));
+		System.out.print("Input your Email here : ");
+		String email=scan.next();
+		boolean check = false;
+		
+		while(readFile.hasNext())
+		{
+			readFile.next();
+			readFile.next();
+			String password = readFile.next();
+			String Email =readFile.next().toUpperCase();
+			
+				if(email.equalsIgnoreCase(Email))
+				{
+					System.out.print("Your password is : " + password);
+					check = true;
+					
+					if(check)
+					{
+						break;
+					}
+				}
+					else
+						check=false;
+		}
+		
+		if(check==false)
+			{
+				System.out.print("The data not found...");
+			}
+		readFile.close();
 	}
-
 }
